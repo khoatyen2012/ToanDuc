6a1d6e4a54acf431f54274d7b496c3c815cafc93
Huy Vu <dinhthitampro@gmail.com>
2018-07-09 23:19:34 +0700

    Thay ID aDmob

--- Assets/Scripts/Cls/Config.cs
+++ Assets/Scripts/Cls/Config.cs
@@ -6,10 +6,9 @@ public class Config  {
 
 #if UNITY_IPHONE
  
-       public static string adsInIDGameOver = "ca-app-pub-5148482490300491/2810940969";
-    public static string adsInIDTrigger = "ca-app-pub-5148482490300491/2810940969";
-    public static string adsInIDBanner = "ca-app-pub-5148482490300491/2810940969";
-     public static string hedieuhanh="ios5";
+       public static string adsInIDGameOver = "ca-app-pub-5148482490300491/7257520601";
+    public static string adsInIDTrigger = "ca-app-pub-5148482490300491/7054340624";
+    public static string adsInIDBanner = "ca-app-pub-5148482490300491/4977582443";
    
 
 #endif
@@ -17,9 +16,9 @@ public class Config  {
 #if UNITY_ANDROID
 
 
-    public static string adsInIDGameOver = "ca-app-pub-5148482490300491/2329302968";
-    public static string adsInIDTrigger = "ca-app-pub-5148482490300491/7589850968";
-    public static string adsInIDBanner = "ca-app-pub-5148482490300491/3578453764";
+    public static string adsInIDGameOver = "ca-app-pub-5148482490300491/7257520601";
+    public static string adsInIDTrigger = "ca-app-pub-5148482490300491/7054340624";
+    public static string adsInIDBanner = "ca-app-pub-5148482490300491/4977582443";
                         
 
 #endif
